896170f2433cd263c8e93b163f6f52d3865547df
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:26:32 +0600

    feat: refactor TrialBalance component to improve structure and include header, table, and footer

--- src/app/(features)/(finance & accounts)/trial-balance/page.tsx
+++ src/app/(features)/(finance & accounts)/trial-balance/page.tsx
@@ -1,7 +1,17 @@
-import TrailbalanceComponent from "@/components/FinanceAccounts/trail-balance/trailbalance";
+import PageHeader from "@/components/FinanceAccounts/TrialBalance/PageHeader";
+import TrialBalanceTable from "@/components/FinanceAccounts/TrialBalance/TrialBalanceTable";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
-export default function TrialBalanace(){
-    return(
-        <><TrailbalanceComponent /></>
-    )
+export default function TrialBalance() {
+  return (
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <TrialBalanceTable />
+        </div>
+        <CommonFooter />
+      </div>
+    </div>
+  );
 }
