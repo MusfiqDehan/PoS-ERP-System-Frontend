4d912465c7d511e799a9513bc07f67c50d66bd95
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:25:32 +0600

    feat: refactor BalanceSheet component to improve structure and include header, table, and footer

--- src/app/(features)/(finance & accounts)/balance-sheet/page.tsx
+++ src/app/(features)/(finance & accounts)/balance-sheet/page.tsx
@@ -1,9 +1,15 @@
-import BalancesheetComponent from "@/components/FinanceAccounts/balance-sheet/balancesheet";
+import PageHeader from "@/components/FinanceAccounts/BalanceSheet/PageHeader";
+import BalanceSheetTable from "@/components/FinanceAccounts/BalanceSheet/BalanceSheetTable";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
-export default function BalanaceSheet() {
+export default function BalanceSheet() {
   return (
-    <>
-      <BalancesheetComponent />
-    </>
+    <div className="page-wrapper">
+      <div className="content">
+        <PageHeader />
+        <BalanceSheetTable />
+      </div>
+      <CommonFooter />
+    </div>
   );
 }
