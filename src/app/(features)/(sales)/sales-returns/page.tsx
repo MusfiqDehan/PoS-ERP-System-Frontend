35c9d6549891e20d368f272eda59583a6f922bbe
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 13:11:58 +0600

    feat: refactor SaleReturn component to improve structure and include header, table, and footer

--- src/app/(features)/(sales)/sales-returns/page.tsx
+++ src/app/(features)/(sales)/sales-returns/page.tsx
@@ -1,9 +1,23 @@
-import SalesReturnComponent from "@/components/sales/sale-return/salesReturn";
+import PageFooter from "@/components/sales/sale-return/PageFooter";
+import PageHeader from "@/components/sales/sale-return/PageHeader";
+import SalesReturnTable from "@/components/sales/sale-return/SalesReturnTable";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import AddSalesReturns from "@/core/modals/sales/addsalesreturns";
+import EditSalesRetuens from "@/core/modals/sales/editsalesretuens";
 
 export default function SaleReturn() {
   return (
-    <>
-      <SalesReturnComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <SalesReturnTable />
+        </div>
+        <PageFooter />
+      </div>
+      <AddSalesReturns />
+      <EditSalesRetuens />
+      <CommonDeleteModal />
+    </div>
   );
 }
