e11c7daa60dc3c7a02216a5e17382194fdd38f2e
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:26:06 +0600

    feat: refactor Income component to improve structure and include header, table, and modals

--- src/app/(features)/(finance & accounts)/income/page.tsx
+++ src/app/(features)/(finance & accounts)/income/page.tsx
@@ -1,9 +1,23 @@
-import IncomeListComponent from "@/components/FinanceAccounts/income/incomeList";
+import AddIncomeModal from "@/components/FinanceAccounts/Income/AddIncomeModal";
+import DeleteIncomeModal from "@/components/FinanceAccounts/Income/DeleteIncomeModal";
+import EditIncomeModal from "@/components/FinanceAccounts/Income/EditIncomeModal";
+import IncomeTable from "@/components/FinanceAccounts/Income/IncomeTable";
+import PageFooter from "@/components/FinanceAccounts/Income/PageFooter";
+import PageHeader from "@/components/FinanceAccounts/Income/PageHeader";
 
 export default function Income() {
   return (
     <>
-      <IncomeListComponent />
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <IncomeTable />
+        </div>
+        <PageFooter />
+      </div>
+      <AddIncomeModal />
+      <EditIncomeModal />
+      <DeleteIncomeModal />
     </>
   );
 }
