4eda289111c2128135ef43340197502046ff60fc
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 14:10:59 +0600

    feat: refactor StockTransfer component to improve structure and include header, table, and footer

--- src/app/(features)/(stock)/stock-transfer/page.tsx
+++ src/app/(features)/(stock)/stock-transfer/page.tsx
@@ -1,9 +1,25 @@
-import StockTransferComponent from "@/components/stock/stock-transfer/stocktransfer";
+import AddTransferModal from "@/components/stock/stock-transfer/AddTransferModal";
+import EditTransferModal from "@/components/stock/stock-transfer/EditTransferModal";
+import ImportTransferModal from "@/components/stock/stock-transfer/ImportTransferModal";
+import PageHeader from "@/components/stock/stock-transfer/PageHeader";
+import StockTransferTable from "@/components/stock/stock-transfer/StockTransferTable";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function StockTransfer() {
   return (
-    <>
-      <StockTransferComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <StockTransferTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <CommonDeleteModal />
+      <AddTransferModal />
+      <EditTransferModal />
+      <ImportTransferModal />
+    </div>
   );
 }
