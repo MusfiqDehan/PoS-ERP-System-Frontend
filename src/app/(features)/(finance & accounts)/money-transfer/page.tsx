fe2f30aa05db25fd21d0258185d3a27285904078
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:26:24 +0600

    feat: refactor MoneyTransfer component to improve structure and include header, table, and modals

--- src/app/(features)/(finance & accounts)/money-transfer/page.tsx
+++ src/app/(features)/(finance & accounts)/money-transfer/page.tsx
@@ -1,9 +1,23 @@
-import MoneytransferComponent from "@/components/FinanceAccounts/money-transfer/moneytransfer";
+import AddMoneyTransferModal from "@/components/FinanceAccounts/MoneyTransfer/AddMoneyTransferModal";
+import DeleteMoneyTransferModal from "@/components/FinanceAccounts/MoneyTransfer/DeleteMoneyTransferModal";
+import EditMoneyTransferModal from "@/components/FinanceAccounts/MoneyTransfer/EditMoneyTransferModal";
+import MoneyTransferTable from "@/components/FinanceAccounts/MoneyTransfer/MoneyTransferTable";
+import PageHeader from "@/components/FinanceAccounts/MoneyTransfer/PageHeader";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
-export default function MonayTransfer() {
+export default function MoneyTransfer() {
   return (
     <>
-      <MoneytransferComponent />
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <MoneyTransferTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <AddMoneyTransferModal />
+      <EditMoneyTransferModal />
+      <DeleteMoneyTransferModal />
     </>
   );
 }
