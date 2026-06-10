4215c745a10641210bdf56b1deee20b3dfd40c83
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:25:19 +0600

    feat: refactor AccountList component to include new layout and additional components

--- src/app/(features)/(finance & accounts)/account-list/page.tsx
+++ src/app/(features)/(finance & accounts)/account-list/page.tsx
@@ -1,9 +1,23 @@
-import AccountlistComponent from "@/components/FinanceAccounts/account-list/accountlist";
+import AccountListModals from "@/components/FinanceAccounts/AccountList/AccountListModals";
+import AccountListTab from "@/components/FinanceAccounts/AccountList/AccountListTab";
+import AccountTypeTab from "@/components/FinanceAccounts/AccountList/AccountTypeTab";
+import TabNav from "@/components/FinanceAccounts/AccountList/TabNav";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function AccountList() {
   return (
-    <>
-      <AccountlistComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <TabNav />
+          <div className="tab-content" id="pills-tabContent">
+            <AccountListTab />
+            <AccountTypeTab />
+          </div>
+        </div>
+        <CommonFooter />
+      </div>
+      <AccountListModals />
+    </div>
   );
 }
