d66f9025d5637747337b46d1d99e8b6c77fd3c20
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:34:41 +0600

    feat: refactor InvoiceDetails component to improve structure and include header, content, and footer

--- src/app/(features)/(sales)/invoice-details/page.tsx
+++ src/app/(features)/(sales)/invoice-details/page.tsx
@@ -1,7 +1,17 @@
-import InvoiceDetailsComponent from "@/components/sales/invoice/invoiceDetails";
+import InvoiceActionButtons from "@/components/sales/invoice-details/InvoiceActionButtons";
+import InvoiceDetailsCard from "@/components/sales/invoice-details/InvoiceDetailsCard";
+import PageFooter from "@/components/sales/invoice-details/PageFooter";
+import PageHeader from "@/components/sales/invoice-details/PageHeader";
 
-export default function InvoiceDetails(){
-    return(
-        <><InvoiceDetailsComponent/></>
-    )
+export default function InvoiceDetails() {
+  return (
+    <div className="page-wrapper">
+      <div className="content">
+        <PageHeader />
+        <InvoiceDetailsCard />
+        <InvoiceActionButtons />
+      </div>
+      <PageFooter />
+    </div>
+  );
 }
