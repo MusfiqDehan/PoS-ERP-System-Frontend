2b4b996dcd52b7de6722bb76b269d9212eb2fff6
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:25:45 +0600

    feat: refactor ExpenseCategory component to improve structure and include header, table, and modals

--- src/app/(features)/(finance & accounts)/expense-category/page.tsx
+++ src/app/(features)/(finance & accounts)/expense-category/page.tsx
@@ -1,9 +1,29 @@
-import ExpenseCategoryComponent from "@/components/FinanceAccounts/expensecategory";
+import AddExpenseCategoryModal from "@/components/FinanceAccounts/ExpenseCategory/AddExpenseCategoryModal";
+import AddStoreModal from "@/components/FinanceAccounts/ExpenseCategory/AddStoreModal";
+import DeleteExpenseModal from "@/components/FinanceAccounts/ExpenseCategory/DeleteExpenseModal";
+import EditExpenseCategoryModal from "@/components/FinanceAccounts/ExpenseCategory/EditExpenseCategoryModal";
+import EditStoreModal from "@/components/FinanceAccounts/ExpenseCategory/EditStoreModal";
+import ExpenseCategoryTable from "@/components/FinanceAccounts/ExpenseCategory/ExpenseCategoryTable";
+import PageFooter from "@/components/FinanceAccounts/ExpenseCategory/PageFooter";
+import PageHeader from "@/components/FinanceAccounts/ExpenseCategory/PageHeader";
 
 export default function ExpenseCategory() {
   return (
     <>
-      <ExpenseCategoryComponent />
+      <div>
+        <div className="page-wrapper">
+          <div className="content">
+            <PageHeader />
+            <ExpenseCategoryTable />
+          </div>
+          <PageFooter />
+          <AddStoreModal />
+          <EditStoreModal />
+        </div>
+        <AddExpenseCategoryModal />
+        <EditExpenseCategoryModal />
+        <DeleteExpenseModal />
+      </div>
     </>
   );
 }
