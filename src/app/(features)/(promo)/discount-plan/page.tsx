67752034b287e0da3e0b7873169a8c157d874df9
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:26:52 +0600

    feat: refactor DiscountPlan component to improve structure and include header, table, and footer

--- src/app/(features)/(promo)/discount-plan/page.tsx
+++ src/app/(features)/(promo)/discount-plan/page.tsx
@@ -1,9 +1,19 @@
-import DiscountPlanComponent from "@/components/promo/discountPlan";
+import DiscountPlanTable from "@/components/promo/discount-plan/DiscountPlanTable";
+import PageHeader from "@/components/promo/discount-plan/PageHeader";
+import DiscountPlanModal from "@/core/modals/coupons/discountPlanModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function DiscountPlan() {
   return (
-    <>
-      <DiscountPlanComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <DiscountPlanTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <DiscountPlanModal />
+    </div>
   );
 }
