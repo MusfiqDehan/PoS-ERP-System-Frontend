cb60c1feca0d81fd3d90c343950d5391b300e9e2
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:27:43 +0600

    feat: refactor Invoice component to improve structure and include header, table, and footer

--- src/app/(features)/(sales)/invoice/page.tsx
+++ src/app/(features)/(sales)/invoice/page.tsx
@@ -1,9 +1,19 @@
-import InvoiceComponent from "@/components/sales/invoice/invoice";
+import InvoiceTable from "@/components/sales/invoice/InvoiceTable";
+import PageHeader from "@/components/sales/invoice/PageHeader";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function Invoice() {
   return (
-    <>
-      <InvoiceComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <InvoiceTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <CommonDeleteModal />
+    </div>
   );
 }
