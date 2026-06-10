31ddd8aaeda0eb3121e1b52bf05e4d9057403fa8
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:27:13 +0600

    feat: refactor PurchaseOrderReport component to improve structure and include header, table, and footer

--- src/app/(features)/(purchases)/purchase-order-report/page.tsx
+++ src/app/(features)/(purchases)/purchase-order-report/page.tsx
@@ -1,9 +1,17 @@
-import PurchaseOrderReportComponent from "@/components/purchase/purchaseOrderReport";
+import PageHeader from "@/components/purchase/purchase-order-report/PageHeader";
+import PurchaseOrderReportTable from "@/components/purchase/purchase-order-report/PurchaseOrderReportTable";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function PurchaseOrderReport() {
   return (
-    <>
-      <PurchaseOrderReportComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <PurchaseOrderReportTable />
+        </div>
+        <CommonFooter />
+      </div>
+    </div>
   );
 }
