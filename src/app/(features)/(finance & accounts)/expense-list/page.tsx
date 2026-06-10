a1cdf3d9e24b034fe5102c983f0356a3b037f4e5
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:25:59 +0600

    feat: refactor ExpenseList component to improve structure and include header, table, and modals

--- src/app/(features)/(finance & accounts)/expense-list/page.tsx
+++ src/app/(features)/(finance & accounts)/expense-list/page.tsx
@@ -1,7 +1,23 @@
-import ExpensesListComponent from "@/components/FinanceAccounts/expenseslist";
+import AddExpenseModal from "@/components/FinanceAccounts/ExpenseList/AddExpenseModal";
+import DeleteExpenseModal from "@/components/FinanceAccounts/ExpenseList/DeleteExpenseModal";
+import EditExpenseModal from "@/components/FinanceAccounts/ExpenseList/EditExpenseModal";
+import ExpenseListTable from "@/components/FinanceAccounts/ExpenseList/ExpenseListTable";
+import PageFooter from "@/components/FinanceAccounts/ExpenseList/PageFooter";
+import PageHeader from "@/components/FinanceAccounts/ExpenseList/PageHeader";
 
-export default function ExpenseList(){
-    return(
-        <><ExpensesListComponent /></>
-    )
+export default function ExpenseList() {
+  return (
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <ExpenseListTable />
+        </div>
+        <PageFooter />
+      </div>
+      <DeleteExpenseModal />
+      <AddExpenseModal />
+      <EditExpenseModal />
+    </div>
+  );
 }
