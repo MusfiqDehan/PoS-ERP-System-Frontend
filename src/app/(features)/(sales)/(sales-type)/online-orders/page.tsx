80ee88bcd06d1c16dffb36b865b40df60835fa3c
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:27:29 +0600

    feat: refactor OnlineOrder component to improve structure and include header, table, and modals

--- src/app/(features)/(sales)/(sales-type)/online-orders/page.tsx
+++ src/app/(features)/(sales)/(sales-type)/online-orders/page.tsx
@@ -1,9 +1,21 @@
-import OnlineOrdersComponent from "@/components/sales/online-orders/onlineOrders";
+import OnlineOrderTable from "@/components/sales/online-orders/OnlineOrderTable";
+import PageHeader from "@/components/sales/online-orders/PageHeader";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
+import OnlineorderModal from "@/components/sales/online-orders/onlineorderModal";
 
 export default function OnlineOrder() {
   return (
-    <>
-      <OnlineOrdersComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <OnlineOrderTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <OnlineorderModal />
+      <CommonDeleteModal />
+    </div>
   );
 }
