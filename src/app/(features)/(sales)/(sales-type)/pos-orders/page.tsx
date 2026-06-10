cec0b2af65df1710972ed8fde725428147461e61
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:27:36 +0600

    feat: refactor PosOrders component to improve structure and include header, table, and modals

--- src/app/(features)/(sales)/(sales-type)/pos-orders/page.tsx
+++ src/app/(features)/(sales)/(sales-type)/pos-orders/page.tsx
@@ -1,9 +1,21 @@
-import PosOrderComponent from "@/components/sales/pos-orders/posOrder";
+import PageHeader from "@/components/sales/pos-orders/PageHeader";
+import PosOrderTable from "@/components/sales/pos-orders/PosOrderTable";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
+import OnlineorderModal from "@/components/sales/online-orders/onlineorderModal";
 
 export default function PosOrders() {
   return (
-    <>
-      <PosOrderComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <PosOrderTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <OnlineorderModal />
+      <CommonDeleteModal />
+    </div>
   );
 }
