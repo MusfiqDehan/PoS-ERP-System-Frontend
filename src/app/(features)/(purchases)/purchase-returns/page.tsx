54bbd60146100b364a3b5fd21cef3f83402d5b27
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:27:20 +0600

    feat: refactor PurchaseReturn component to improve structure and include header, table, and footer

--- src/app/(features)/(purchases)/purchase-returns/page.tsx
+++ src/app/(features)/(purchases)/purchase-returns/page.tsx
@@ -1,9 +1,23 @@
-import PurchaseReturnComponent from "@/components/purchase/purchase-returns";
+import PageHeader from "@/components/purchase/purchase-returns/PageHeader";
+import PurchaseReturnTable from "@/components/purchase/purchase-returns/PurchaseReturnTable";
+import AddPurchaseReturn from "@/core/modals/purchases/addpurchasereturn";
+import EditPurchaseReturns from "@/core/modals/purchases/editpurchasereturns";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function PurchaseReturn() {
   return (
-    <>
-      <PurchaseReturnComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <PurchaseReturnTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <AddPurchaseReturn />
+      <EditPurchaseReturns />
+      <CommonDeleteModal />
+    </div>
   );
 }
