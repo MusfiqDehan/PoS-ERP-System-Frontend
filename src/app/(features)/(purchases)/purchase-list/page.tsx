e8ac7f5a53090b28381add7427d57a02e15c25d2
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:27:06 +0600

    feat: refactor PurchaseList component to improve structure and include header, table, and modals

--- src/app/(features)/(purchases)/purchase-list/page.tsx
+++ src/app/(features)/(purchases)/purchase-list/page.tsx
@@ -1,9 +1,25 @@
-import PurchaseListComponent from "@/components/purchase/purchaselist";
+import PageHeader from "@/components/purchase/purchase-list/PageHeader";
+import PurchaseListTable from "@/components/purchase/purchase-list/PurchaseListTable";
+import AddPurchases from "@/core/modals/purchases/addpurchases";
+import EditPurchases from "@/core/modals/purchases/editpurchases";
+import ImportPurchases from "@/core/modals/purchases/importpurchases";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function PurchaseList() {
   return (
-    <>
-      <PurchaseListComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <PurchaseListTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <AddPurchases />
+      <ImportPurchases />
+      <EditPurchases />
+      <CommonDeleteModal />
+    </div>
   );
 }
