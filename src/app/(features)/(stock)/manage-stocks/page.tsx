881e45d1400882d425a981942c5fff9cabc6df35
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 14:10:36 +0600

    feat: refactor ManageStock component to improve structure and include header, table, and footer

--- src/app/(features)/(stock)/manage-stocks/page.tsx
+++ src/app/(features)/(stock)/manage-stocks/page.tsx
@@ -1,9 +1,23 @@
-import ManageStockComponent from "@/components/stock/managestock/managestock";
+import AddStockModal from "@/components/stock/managestock/AddStockModal";
+import EditStockModal from "@/components/stock/managestock/EditStockModal";
+import ManageStockTable from "@/components/stock/managestock/ManageStockTable";
+import PageHeader from "@/components/stock/managestock/PageHeader";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function ManageStock() {
   return (
-    <>
-      <ManageStockComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <ManageStockTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <CommonDeleteModal />
+      <AddStockModal />
+      <EditStockModal />
+    </div>
   );
 }
