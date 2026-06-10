3a8d1ca0410ea5bc0ca5ec3c29d0acc7e6137fd9
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:26:39 +0600

    feat: refactor Coupons component to improve structure and include header, table, and modals

--- src/app/(features)/(promo)/coupons/page.tsx
+++ src/app/(features)/(promo)/coupons/page.tsx
@@ -1,7 +1,21 @@
-import CouponsComponent from "@/components/promo/coupons";
+import CouponTable from "@/components/promo/coupons/CouponTable";
+import PageHeader from "@/components/promo/coupons/PageHeader";
+import AddCoupons from "@/core/modals/coupons/addcoupons";
+import EditCoupons from "@/core/modals/coupons/editcoupons";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
-export default function Coupons(){
-    return(
-        <><CouponsComponent /></>
-    )
+export default function Coupons() {
+  return (
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <CouponTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <AddCoupons />
+      <EditCoupons />
+    </div>
+  );
 }
