adbff378fc6bd884c8bca940bda8423fdd73f8cc
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:42:54 +0600

    feat: refactor Quotation component to improve structure and include header, table, and footer

--- src/app/(features)/(sales)/quotation-list/page.tsx
+++ src/app/(features)/(sales)/quotation-list/page.tsx
@@ -1,9 +1,23 @@
-import QuotationComponent from "@/components/sales/quotation/quotation";
+import PageHeader from "@/components/sales/quotation/PageHeader";
+import QuotationTable from "@/components/sales/quotation/QuotationTable";
+import CommonDeleteModal from "@/core/common/modal/commonDeleteModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
+import AddQuotation from "@/core/modals/sales/addquotation";
+import EditQuotation from "@/core/modals/sales/editquotation";
 
 export default function Quotation() {
   return (
-    <>
-      <QuotationComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <QuotationTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <AddQuotation />
+      <EditQuotation />
+      <CommonDeleteModal />
+    </div>
   );
 }
