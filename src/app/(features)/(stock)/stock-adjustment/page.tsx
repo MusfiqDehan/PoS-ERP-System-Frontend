321a30717f24956f1e6dea9ae68617cd35121944
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 14:10:47 +0600

    feat: refactor StockAdjustment component to improve structure and include header, table, and footer

--- src/app/(features)/(stock)/stock-adjustment/page.tsx
+++ src/app/(features)/(stock)/stock-adjustment/page.tsx
@@ -1,9 +1,23 @@
-import StockAdjustmentComponent from "@/components/stock/stock-adjustment/stockAdjustment";
+import AddAdjustmentModal from "@/components/stock/stock-adjustment/AddAdjustmentModal";
+import EditAdjustmentModal from "@/components/stock/stock-adjustment/EditAdjustmentModal";
+import PageHeader from "@/components/stock/stock-adjustment/PageHeader";
+import StockAdjustmentTable from "@/components/stock/stock-adjustment/StockAdjustmentTable";
+import ViewNotesModal from "@/components/stock/stock-adjustment/ViewNotesModal";
+import CommonFooter from "@/core/common/footer/commonFooter";
 
 export default function StockAdjustment() {
   return (
-    <>
-      <StockAdjustmentComponent />
-    </>
+    <div>
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <StockAdjustmentTable />
+        </div>
+        <CommonFooter />
+      </div>
+      <AddAdjustmentModal />
+      <EditAdjustmentModal />
+      <ViewNotesModal />
+    </div>
   );
 }
