c1e59867f839a9693b221c8273e52ed098b2a3a7
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-10 12:26:16 +0600

    feat: refactor IncomeCategory component to improve structure and include header, table, and modals

--- src/app/(features)/(finance & accounts)/income-category/page.tsx
+++ src/app/(features)/(finance & accounts)/income-category/page.tsx
@@ -1,9 +1,23 @@
-import IncomeCategoryComponent from "@/components/FinanceAccounts/income/incomeCategory";
+import AddIncomeCategoryModal from "@/components/FinanceAccounts/IncomeCategory/AddIncomeCategoryModal";
+import DeleteIncomeCategoryModal from "@/components/FinanceAccounts/IncomeCategory/DeleteIncomeCategoryModal";
+import EditIncomeCategoryModal from "@/components/FinanceAccounts/IncomeCategory/EditIncomeCategoryModal";
+import IncomeCategoryTable from "@/components/FinanceAccounts/IncomeCategory/IncomeCategoryTable";
+import PageFooter from "@/components/FinanceAccounts/IncomeCategory/PageFooter";
+import PageHeader from "@/components/FinanceAccounts/IncomeCategory/PageHeader";
 
 export default function IncomeCategory() {
   return (
     <>
-      <IncomeCategoryComponent />
+      <div className="page-wrapper">
+        <div className="content">
+          <PageHeader />
+          <IncomeCategoryTable />
+        </div>
+        <PageFooter />
+      </div>
+      <AddIncomeCategoryModal />
+      <EditIncomeCategoryModal />
+      <DeleteIncomeCategoryModal />
     </>
   );
 }
