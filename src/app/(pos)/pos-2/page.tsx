3966a8ab921ea5e5752004a70e6de82c06cc0325
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-06-13 09:59:05 +0600

    refactor: improve Pos2 component structure and formatting

--- src/app/(pos)/pos-2/page.tsx
+++ src/app/(pos)/pos-2/page.tsx
@@ -1,7 +1,9 @@
 import Pos2Component from "@/components/pos-module/pos/pos2";
 
-export default function Pos2(){
-    return(
-        <><Pos2Component /></>
-    )
+export default function Pos2() {
+  return (
+    <>
+      <Pos2Component />
+    </>
+  );
 }
