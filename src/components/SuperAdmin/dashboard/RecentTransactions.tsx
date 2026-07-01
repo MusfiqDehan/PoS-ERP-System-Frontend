2b06cb756051c9493a454911f69e3941e250f320
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-07-01 16:16:58 +0600

    refactor: update RecentTransactions component link for improved navigation
    Changed the link in the RecentTransactions component to direct users to the correct purchase transaction route. This update aims to enhance user navigation and ensure accurate access to transaction details.

--- src/components/SuperAdmin/dashboard/RecentTransactions.tsx
+++ src/components/SuperAdmin/dashboard/RecentTransactions.tsx
@@ -11,7 +11,7 @@ export default function RecentTransactions() {
               <div className="card-header pb-2 d-flex align-items-center justify-content-between flex-wrap">
                 <h5 className="mb-2 custome-heading">Recent Transactions</h5>
                 <Link
-                  href={routes.superAdminPurchaseTransaction}
+                  href={routes.purchasetransaction}
                   className="btn btn-light btn-md mb-2"
                 >
                   View All
