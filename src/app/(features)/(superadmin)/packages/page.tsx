a66027899a4bf13ed05a5ca24fa30749e6858c36
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-07-01 16:12:18 +0600

    feat: add search functionality to Packages component
    Enhanced the Packages component by integrating a search feature. The PageHeader now accepts a searchText prop and an onSearchChange callback, while the PackagesTable is updated to utilize the searchText for filtering. This improvement increases usability and efficiency in managing package data.

--- src/app/(features)/(superadmin)/packages/page.tsx
+++ src/app/(features)/(superadmin)/packages/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import CommonFooter from "@/core/common/footer/commonFooter";
 import AddPlanModal from "@/components/SuperAdmin/packages/AddPlanModal";
 import DeletePackageModal from "@/components/SuperAdmin/packages/DeletePackageModal";
@@ -7,13 +10,15 @@ import PageHeader from "@/components/SuperAdmin/packages/PageHeader";
 import StatsCards from "@/components/SuperAdmin/packages/StatsCards";
 
 export default function Package() {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <>
       <div className="page-wrapper">
         <div className="content">
-          <PageHeader />
+          <PageHeader searchText={searchText} onSearchChange={setSearchText} />
           <StatsCards />
-          <PackagesTable />
+          <PackagesTable searchText={searchText} />
         </div>
         <CommonFooter />
       </div>
